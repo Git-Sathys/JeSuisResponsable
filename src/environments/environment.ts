737370a5a9d92bc62364630eba48940d64a7ce85
unknown <mehdibarchi@icloud.com>
2020-09-07 14:38:56 +0200

    environnement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCTtV6pP4XqtPaPZyiZo9mWHYstvLMC1lI',
+    authDomain: 'jsres-18e49.firebaseapp.com',
+    databaseURL: 'https://jsres-18e49.firebaseio.com',
+    projectId: 'jsres-18e49',
+    storageBucket: 'jsres-18e49.appspot.com',
+    messagingSenderId: '975431356563',
+    appId: '1:975431356563:web:289540aa711b4ba37ebd66'
+  }
 };
 
 /*
